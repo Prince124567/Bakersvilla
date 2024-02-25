79310844e61f5b4aa4b76237ec006a586ea08b53
Prince <158798572+Prince124567@users.noreply.github.com>
2024-02-25 10:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementById("navLinks")
+
+        function show(){
+            navLinks.style.right = "0";
+        }
+        function hiden(){
+            navLinks.style.right = "-200px";
+        }
